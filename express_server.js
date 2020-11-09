bca1eea9e2827055f2be4a81d3d8a032ca1ec413
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-09 23:59:35 +0000

    adding ejs + rouyinf to urls

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -7,15 +8,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls", (req, res) => {
+
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
