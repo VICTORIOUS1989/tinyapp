50f01a3f073474e71a228a8bcc742145e31ba965
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-10 03:03:50 +0000

    Redirect After Form Submission

--- express_server.js
+++ express_server.js
@@ -20,16 +20,29 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-app.get("/urls/:shortURL", (req, res) => {
 
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
+app.get("/u/:shortURL", (req, res) => {
+  //console.log(req.params.shortURL);
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+
+  shortURL = generateRandomString();
+  longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  //console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`);         
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  console.log(templateVars);
+  console.log(req.params);
+  res.render("urls_show", templateVars);
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
