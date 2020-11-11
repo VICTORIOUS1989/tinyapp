8ebf31b2f4fbad483a5ebf4717f3b196a908a8fd
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-11-11 01:36:34 +0000

    ading cookies

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser')
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
+
 app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
 
@@ -13,12 +16,15 @@ const urlDatabase = {
 
 app.get("/urls", (req, res) => {
 
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase , username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -38,7 +44,7 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
 
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
   //console.log(templateVars);
   //console.log(req.params);
   res.render("urls_show", templateVars);
@@ -56,6 +62,17 @@ app.post("/urls/:shortURL" ,(req, res) => {
   res.redirect("/urls");
  
   }); 
+
+app.post("/login" ,(req, res) => {
+
+  const username = req.body.username;
+  res.cookie ('username', username);
+  res.redirect('/urls');
+  }); 
+  app.post("/logout" ,(req, res) => {
+    res.clearCookie ('username');
+    res.redirect('/urls');
+    }); 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
